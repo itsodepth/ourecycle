d16ab15bbd065ca9bd9e570bb8fc6a8720773b18
Itsodepth <fuyukiraiko@gmail.com>
2024-12-18 22:18:40 +0700

    update navbar

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ourecycle</title>
+        <!-- Link Font -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+        <!-- Link Custom CSS -->
+        <link rel="stylesheet" href="css/styles.css">
+        <!-- Link Bootstrap -->
+        <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    </head>
+
+    <body>
+        <!-- Bootstrap Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-nav-apple py-3">
+            <div class="container-fluid">
+                <a class="navbar-brand ps-2 ps-sm-5 fw-bold fs-4" href="#">Ourecycle</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto fw-semibold">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item ms-0 mx-sm-3">
+                            <!-- Login Button -->
+                            <a class="btn btn-light px-3" href="login.php">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    </body>
+
+</html>
