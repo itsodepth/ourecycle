58a775fd20641975779b94af5c0bc8264c09191e
Devano Alfarizy <devanoalfarizy04@gmail.com>
2024-12-21 18:51:40 +0700

    tambah banner dan kawannya

--- index.php
+++ index.php
@@ -45,24 +45,27 @@
         </nav>
 
         <!-- Banner Section -->
-        <div id="bannerCarousel" class="carousel slide mt-5 pt-4" data-bs-ride="carousel">
-            <div class="carousel-inner" style="max-height: 580px;">
-                <div class="carousel-item active" style="max-height: 580px;">
-                    <img src="assets/images/banner 1.jpg" class="d-block w-100" alt="Banner 1">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Recycling for Everyone</h5>
+        <div id="bannerCarousel" class="carousel slide mt-4" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="assets/images/banner 1.jpg" class="d-block w-100 img-fluid" alt="Banner 1"
+                        style="object-fit: cover; height: 100vh;">
+                    <div class="carousel-caption text-start justify-content-center d-none d-md-block">
+                        <h5 class="display-1">Recycling for Everyone</h5>
                         <p>#ubahjadikebaikan</p>
                     </div>
                 </div>
-                <div class="carousel-item" style="max-height: 580px;">
-                    <img src="assets/images/banner 2.jpg" class="d-block w-100" alt="Banner 2">
+                <div class="carousel-item">
+                    <img src="assets/images/banner 2.jpg" class="d-block w-100 img-fluid" alt="Banner 2"
+                        style="object-fit: cover; height: 100vh;">
                     <div class="carousel-caption d-none d-md-block">
                         <h5>Make Recycling Easy</h5>
                         <p>Join us in making the world cleaner.</p>
                     </div>
                 </div>
-                <div class="carousel-item" style="max-height: 580px;">
-                    <img src="assets/images/banner 3.jpg" class="d-block w-100" alt="Banner 3">
+                <div class="carousel-item">
+                    <img src="assets/images/banner 3.jpg" class="d-block w-100 img-fluid" alt="Banner 3"
+                        style="object-fit: cover; height: 100vh;">
                     <div class="carousel-caption d-none d-md-block">
                         <h5>Get Rewarded</h5>
                         <p>Earn rewards while saving the planet.</p>
@@ -77,6 +80,45 @@
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
+        </div>
+
+        <!-- Our Mission -->
+        <div class="container mt-5">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <h2>We are The Recycling Network</h2>
+                    <p>#ubahjadikebaikan</p>
+                    <h4>Misi Kami Menyediakan Akses Daur Ulang Bagi Semua Orang</h4>
+                    <p>Teknologi Mallsampah didesain untuk menangkap limbah dari sumber timbulnya, dengan menggunakan
+                        jejaring pengepul dan pemulung lokal sebagai kunci dari rantai daur ulang di Indonesia.</p>
+                </div>
+                <div class="col-md-6">
+                    <img src="assets/images/test.svg" alt="Recycling" class="img-fluid rounded">
+                </div>
+            </div>
+            <div class="row mt-4 text-center">
+                <div class="col">
+                    <div class="row">
+                        <div class="col stat-item">
+                            <h3>1jt Kg+</h3>
+                            <p>Sampah di Daur Ulang</p>
+                        </div>
+                        <div class="col stat-item">
+                            <h3>100+</h3>
+                            <p>Gudang Sortir</p>
+                        </div>
+                        <div class="col stat-item">
+                            <h3>500+</h3>
+                            <p>Kolektor Lokal</p>
+                        </div>
+                        <div class="col stat-item">
+                            <h3>30rb+</h3>
+                            <p>Pengguna</p>
+                        </div>
+                    </div>
+                    <a href="#" class="btn btn-primary mt-3">Semua Tentang Kami di sini</a>
+                </div>
+            </div>
         </div>
 
         <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
